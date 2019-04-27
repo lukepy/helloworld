396b64a4d98ad2c60ba8589e78800c083879607a
lukepy <luke.py@gmail.com>
2019-04-27 09:20:42 -0400

    Add files via upload

---
+++ data_processing_template.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+
+#Import dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values 
+y = dataset.iloc[:,3].values
+
+# Splitting the dataset into the Training set and test set 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#Feature Scaling 
+"""from sklearn.preprocessing import  StandardScaler 
+sc_X = StandardScaler();
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
